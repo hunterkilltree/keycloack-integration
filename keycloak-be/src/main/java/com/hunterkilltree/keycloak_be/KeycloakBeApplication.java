3ce111640a2c777a6e39f6f3378c5f1bd29192c5
hunterkilltree <snguoila@yahoo.com>
2024-09-09 21:56:24 +0700

    Add backend

---
+++ keycloak-be/src/main/java/com/hunterkilltree/keycloak_be/KeycloakBeApplication.java
@@ -0,0 +1,13 @@
+package com.hunterkilltree.keycloak_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakBeApplication.class, args);
+	}
+
+}
---
+++ keycloak-be/src/test/java/com/hunterkilltree/keycloak_be/KeycloakBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunterkilltree.keycloak_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
